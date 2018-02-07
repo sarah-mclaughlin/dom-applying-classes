fd234b1750fdf2158a66217f38e147056f40fde2
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-02-08 12:59:21 +1300

    DOM classes

--- classes.js
+++ classes.js
@@ -7,6 +7,8 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one()
   two()
+  three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -24,6 +26,12 @@ function two () {
   two.classList.add('green')
 }
 
-// CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('orange')
+}
 
-// CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
